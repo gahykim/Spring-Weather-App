5bd488633901db9c6e0310885b98d689cf07bd16
Gahyung Kim <ghkim10202@gmail.com>
2024-06-30 21:07:16 +0900

    upload files

---
+++ src/test/java/zerobase/weather/JpaRepositoryTest.java
@@ -0,0 +1,45 @@
+package zerobase.weather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import zerobase.weather.domain.Memo;
+import zerobase.weather.repository.JpaMemoRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
+// Test code에 transactional은 변경사항을 rollback 처리를 한다.
+@Transactional
+public class JpaRepositoryTest {
+    @Autowired
+    JpaMemoRepository jpaMemoRepository;
+
+    @Test
+    void insertMemoTest(){
+        //given
+        Memo newMemo = new Memo(10, "this is jpa memo");
+        //when
+        jpaMemoRepository.save(newMemo);
+        //then
+        List<Memo> memoList = jpaMemoRepository.findAll();
+        assertTrue(memoList.size() > 0);
+    }
+
+    @Test
+    void findByIdTest(){
+        //given
+        Memo newMemo = new Memo(11, "this is jpa memo2");
+        //when
+        Memo memo = jpaMemoRepository.save(newMemo);
+        //then
+        Optional<Memo> result = jpaMemoRepository.findById(memo.getId());
+        assertEquals(result.get().getText(), "this is jpa memo2");
+    }
+
+}
